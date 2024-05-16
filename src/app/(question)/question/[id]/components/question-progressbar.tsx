f28ce6f298601fe7be93612f186d5a0b0fff0aa4
OHS <ohs6006@gmail.com>
2024-05-16 18:43:09 +0900

    Design : 답변 작성 페이지 프로그레스 바 UI

--- src/app/(question)/question/[id]/components/question-progressbar.tsx
+++ src/app/(question)/question/[id]/components/question-progressbar.tsx
@@ -1,11 +1,26 @@
+"use client";
+
 import * as React from "react";
+import { Progress } from "@/components/ui/progress";
 
 interface IQuestionProgressbarProps {}
 
 const QuestionProgressbar: React.FunctionComponent<
   IQuestionProgressbarProps
 > = (props) => {
-  return <div>QuestionProgressbar</div>;
+  // zustand or props로 지금 현재 문제, 전체 문제 받기
+  const [progress, setProgress] = React.useState(13);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => setProgress(66), 500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className="">
+      <Progress value={progress} className="w-full" />
+    </div>
+  );
 };
 
 export default QuestionProgressbar;
