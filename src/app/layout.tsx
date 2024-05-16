bac1821da983e918456150569374fd63885e199b
JEONGMIN <gyflsakfn@naver.com>
2024-05-16 10:02:50 +0900

    Feat: 라이브러리 및 shadcn 컴포넌트 설치

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
+import ReactQueryProvider from "@/provider/query-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
+      </body>
     </html>
   );
 }
