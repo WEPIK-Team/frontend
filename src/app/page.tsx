ebb643a3007b975f4fc923de73e98f884eb90014
shin jooyoung <young@staybility.co.kr>
2024-05-18 16:18:47 +0900

    Feat: 헤더, 푸터, 레이아웃 컴포넌트 추가 및 SEO 메타 태그 작업
    - 사이트 구조 개선을 위해 헤더, 푸터, 레이아웃 컴포넌트 구현
    - 검색 엔진 가시성을 높이기 위해 SEO 최적화 메타 태그 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
-    <div>
+    <>
       <div className="font-pretendard font-semibold">
         위픽 서비스 프리텐다드
       </div>
       <div className="font-allroundgothic ">Wepik Service All Round Gothic</div>
-    </div>
+    </>
   );
 }
