49efc483b43e9db7c3ad06c67b4e3e6b337a5477
Vee-ble <gyflsakfn@gmail.com>
2024-05-19 13:57:44 +0900

    Refactor: 폰트 loot layout에서 분리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,51 +1,12 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 
 import "@/styles/globals.css";
+import { allRoundGothic, pretendard } from "@/styles/font";
 
 import Layout from "@/components/common/layout";
 
 import { MetaData } from "@/constants/config";
 import ReactQueryProvider from "@/provider/query-provider";
-
-const pretendard = localFont({
-  src: "./fonts/PretendardVariable.woff2",
-  display: "swap",
-  weight: "45 920",
-  variable: "--font-pretendard",
-});
-
-const allRoundGothic = localFont({
-  src: [
-    {
-      path: "./fonts/allroundgothic-bold.otf",
-      weight: "700",
-      style: "bold",
-    },
-    {
-      path: "./fonts/allroundgothic-semi.otf",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "./fonts/allroundgothic-medium.otf",
-      weight: "500",
-      style: "medium",
-    },
-    {
-      path: "./fonts/allroundgothic-text.otf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "./fonts/allroundgothic-thick.otf",
-      weight: "100",
-      style: "thin",
-    },
-  ],
-  display: "swap",
-  variable: "--font-allroundgothic",
-});
 
 export const metadata: Metadata = MetaData;
 
