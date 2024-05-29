c9b1cb5fdac05993b754a9d9b92314421299d068
S-jooyoung <jooyoung.dev@gmail.com>
2024-05-29 16:41:07 +0900

    Design: 푸터 텍스트 스타일 변경
    - 폰트 굵기 부분 변경

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <>
-      <footer className="container w-full max-w-3xl bg-[#F8F7FD] py-4 text-wpt-sm text-wpc-gray">
+      <footer className="container w-full max-w-3xl bg-white py-4 text-wpt-sm font-medium text-wpc-gray">
         <section className="mb-[15px] flex justify-center">
           <Link href="/">
             <Image
@@ -19,15 +19,17 @@ export default function Footer() {
         <section className="mb-[10px] flex justify-center gap-3">
           <Link href="/">서비스 소개</Link>
           <Link href="/">이용약관</Link>
-          <Link href="/">개인정보 처리방침</Link>
+          <Link href="/" className="text-[#65656D]">
+            개인정보 처리방침
+          </Link>
         </section>
         <section className="mb-[10px] flex justify-center gap-3">
-          <p>문의 wepik@wepik.com</p>
+          <p className="font-normal text-[65656D]">문의 wepik@wepik.com</p>
         </section>
-        <section className="mb-[2px] flex justify-center gap-3 text-xs">
+        <section className="mb-[2px] flex justify-center gap-3 text-xs font-light">
           <p>ⓒ 2024. WEPIK. All Rights Reserved.</p>
         </section>
-        <section className="flex justify-center gap-3 text-xs">
+        <section className="flex justify-center gap-3 text-xs font-light">
           <p>[FE] 비블, 조이, 라이언 | [BE] 망구스, 카이 | [DE] 조앤</p>
         </section>
       </footer>
