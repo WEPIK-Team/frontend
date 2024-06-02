18a1122478b583740e1059ae72b77abbd5954193
S-jooyoung <jooyoung.dev@gmail.com>
2024-06-03 00:06:54 +0900

    Fix: 템플릿 선택 페이지 태그, 인기순 정렬 버그 해결

--- src/components/card/template-select-card-list.tsx
+++ src/components/card/template-select-card-list.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 import { TemplateSelectCard } from "@/components/card/template-select-card";
 import { TemplateTag } from "@/components/tag/template-tag";
@@ -19,27 +19,33 @@ export function TemplateSelectCardList({
   const [sortByPopularity, setSortByPopularity] = useState(false);
 
   const handleTagSelect = (tag: string) => {
-    if (selectedTags.includes(tag)) {
-      setSelectedTags(
-        selectedTags.filter((selectedTag) => selectedTag !== tag)
-      );
-    } else {
-      setSelectedTags([...selectedTags, tag]);
-    }
+    setSelectedTags((prevSelectedTags) =>
+      prevSelectedTags.includes(tag)
+        ? prevSelectedTags.filter((selectedTag) => selectedTag !== tag)
+        : [...prevSelectedTags, tag]
+    );
   };
 
-  let filteredTemplateList =
-    selectedTags.length > 0
-      ? templateList.filter((template) =>
-          selectedTags.every((tag) => template.templateTags.includes(tag))
-        )
-      : templateList;
+  const filteredTemplateList = useMemo(() => {
+    let updatedTemplateList =
+      selectedTags.length > 0
+        ? templateList.filter((template) =>
+            selectedTags.every((tag) => template.templateTags.includes(tag))
+          )
+        : templateList;
 
-  if (sortByPopularity) {
-    filteredTemplateList = filteredTemplateList
-      .slice()
-      .sort((a, b) => b.useCount - a.useCount);
-  }
+    if (sortByPopularity) {
+      updatedTemplateList = updatedTemplateList.sort(
+        (a, b) => b.useCount - a.useCount
+      );
+    } else {
+      updatedTemplateList = updatedTemplateList.sort(
+        (a, b) => a.useCount - b.useCount
+      );
+    }
+
+    return updatedTemplateList;
+  }, [selectedTags, sortByPopularity, templateList]);
 
   return (
     <>
@@ -52,14 +58,17 @@ export function TemplateSelectCardList({
       </div>
       <div
         className="flex cursor-pointer items-center justify-end gap-[5px]"
-        onClick={() => setSortByPopularity(!sortByPopularity)}
+        onClick={() => setSortByPopularity((prev) => !prev)}
       >
         <Image src="svgs/sort.svg" alt="sortIcon" width={12} height={12} />
         <p className="text-wpt-base-1">인기순</p>
       </div>
       <div className="grid grid-cols-12 gap-[14px] pt-3 font-pretendard">
-        {filteredTemplateList.map((template) => (
-          <div key={template.title} className="col-span-6 md:col-span-4">
+        {filteredTemplateList.map((template, index) => (
+          <div
+            key={`${template.title}-${index}`}
+            className="col-span-6 md:col-span-4"
+          >
             <TemplateSelectCard
               id={template.id}
               imageURL={template.imageURL}
