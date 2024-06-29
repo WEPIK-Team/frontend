b3b7818a4373e6d1e7a79a2e8e3a348dff83eaf0
OHS <ohs6006@gmail.com>
2024-06-29 22:00:38 +0900

    Chore : SelectItem 아이콘 버그 수정 & Slider ReadOnly UI 적용

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,4 +1,16 @@
+"use client";
+
+import QuestionLabel from "@/components/question/question-label";
+import { Slider } from "@/components/ui/slider";
+
 const ButtonPage = () => {
-  return <div className="mx-auto max-w-3xl space-y-3 py-5 "></div>;
+  return (
+    <div className="mx-auto max-w-3xl space-y-3 py-5 ">
+      <QuestionLabel color="sender">보낸사람</QuestionLabel>
+      <Slider value={[70]} theme="sender" readonly />
+      <QuestionLabel color="receiver">받는사람</QuestionLabel>
+      <Slider value={[85]} theme="receiver" />
+    </div>
+  );
 };
 export default ButtonPage;
--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -10,18 +10,26 @@ import SpeechBubble from "@/components/question/speech-bubble";
 interface SliderProps
   extends React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root> {
   theme: "sender" | "receiver" | "default";
+  readonly?: boolean;
 }
 
 const ThemeObj = {
-  sender: "bg-wpc-second-grad",
-  receiver: "bg-wpc-main2-grad",
-  default: "bg-wpc-primary-grad",
+  bg: {
+    sender: "bg-wpc-second-grad",
+    receiver: "bg-wpc-main2-grad",
+    default: "bg-wpc-primary-grad",
+  },
+  textColor: {
+    sender: "text-wpc-primary",
+    receiver: "text-wpc-second",
+    default: "text-wpc-gray",
+  },
 };
 
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
   SliderProps
->(({ className, theme = "default", ...props }, ref) => (
+>(({ className, theme = "default", readonly, ...props }, ref) => (
   <SliderPrimitive.Root
     ref={ref}
     className={cn(
@@ -32,14 +40,36 @@ const Slider = React.forwardRef<
   >
     <SliderPrimitive.Track className="relative h-[12px] w-full grow overflow-hidden rounded-full bg-wpc-gray2">
       <SliderPrimitive.Range
-        className={cn("absolute h-full", ThemeObj[theme] || "bg-wpc-gray")}
+        className={cn("absolute h-full", ThemeObj.bg[theme] || "bg-wpc-gray")}
       />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="relative block h-[28px] w-[28px] rounded-full bg-background shadow-md transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-wpc-primary disabled:pointer-events-none disabled:opacity-50">
-      <SpeechBubble className="absolute -left-[20px] -top-[50px]">
-        {props.value}%
-      </SpeechBubble>
-    </SliderPrimitive.Thumb>
+    {readonly ? (
+      <>
+        <span
+          className={cn(
+            " ml-[10px] text-wpt-base-1 font-semibold",
+            ThemeObj.textColor[theme]
+          )}
+        >
+          {props.value}%
+        </span>
+        <SliderPrimitive.Thumb
+          className={cn(
+            "relative mr-10 block h-[28px] w-[28px] rounded-full bg-background shadow-md transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-wpc-primary disabled:pointer-events-none disabled:opacity-50"
+          )}
+        ></SliderPrimitive.Thumb>
+      </>
+    ) : (
+      <SliderPrimitive.Thumb
+        className={cn(
+          "relative block h-[28px] w-[28px] rounded-full bg-background shadow-md transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-wpc-primary disabled:pointer-events-none disabled:opacity-50"
+        )}
+      >
+        <SpeechBubble className="absolute -left-[20px] -top-[50px]">
+          {props.value}%
+        </SpeechBubble>
+      </SliderPrimitive.Thumb>
+    )}
   </SliderPrimitive.Root>
 ));
 Slider.displayName = SliderPrimitive.Root.displayName;
